ff2373a5cdcff2b4619a8c37c94da7349cf45c49
Jason Bahl <jasonbahl@mac.com>
2021-02-24 21:06:11 -0700

    Scaffold Home Page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,29 @@
 import * as React from "react";
+import { Link as GatsbyLink } from 'gatsby'
+import { Container, Stack, Box, Heading, Text, Link } from '@chakra-ui/react'
 
-const HomePage = () => (
-    <div>
-        <h1>Hello World!!</h1>
-    </div>
-)
+const HomePage = () => {
+  return(
+      <Container maxW="xl" centerContent>
+        <Heading as={`h1`} m={5}>#100DaysOfGatsby</Heading>
+        <Stack spacing={8}>
+          <Box p={5} shadow="md" borderWidth="1px">
+            <Heading as={`h2`}>Hello World</Heading>
+            <Text mt={4}>
+              <span dangerouslySetInnerHTML={{__html: "<p>This is an excerpt.</p>"}} />
+            </Text>
+            <Link as={GatsbyLink} to="#">Read More >></Link>
+          </Box>
+          <Box p={5} shadow="md" borderWidth="1px">
+            <Heading as={`h2`}>Goodbye Mars</Heading>
+            <Text mt={4}>
+              <span dangerouslySetInnerHTML={{__html: "<p>This is another excerpt.</p>"}} />
+            </Text>
+            <Link as={GatsbyLink} to="#">Read More >></Link>
+          </Box>
+        </Stack>
+      </Container>
+  )
+}
 
-export default HomePage;
+export default HomePage
